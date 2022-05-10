22b17a2f655fa2f7bec51b9bfa4d569e5223c1b2
Kamil Mrzygłód <kamil@thecloudtheory.com>
2022-05-10 15:23:51 +0200

    Added function app

--- webapp.tf
+++ webapp.tf
@@ -13,11 +13,11 @@ resource "azurerm_linux_web_app" "training" {
   service_plan_id     = azurerm_service_plan.training.id
 
   app_settings = {
-    "InstrumentationKey" = azurerm_application_insights.training.instrumentation_key
+    "APPINSIGHTS_INSTRUMENTATIONKEY" = azurerm_application_insights.training.instrumentation_key
   }
 
   site_config {
-    
+
   }
 }
 
@@ -26,4 +26,30 @@ resource "azurerm_application_insights" "training" {
   location            = azurerm_resource_group.training.location
   resource_group_name = azurerm_resource_group.training.name
   application_type    = "web"
+}
+
+resource "azurerm_linux_function_app" "training" {
+  name                 = "kamilm-tf-functionapp"
+  resource_group_name  = azurerm_resource_group.training.name
+  location             = azurerm_resource_group.training.location
+  service_plan_id      = azurerm_service_plan.training.id
+  storage_account_name = "kmfunctionstorage"
+
+  app_settings = {
+    "APPINSIGHTS_INSTRUMENTATIONKEY" = azurerm_application_insights.training.instrumentation_key
+  }
+
+  site_config {
+      application_stack {
+        dotnet_version = "6.0"
+      }
+  }
+}
+
+module "function-app-storage-account" {
+  source = "./modules/storage-account"
+
+  storage_account_name = "kmfunctionstoragetf"
+  resource_group_name  = azurerm_resource_group.training.name
+  location             = azurerm_resource_group.training.location
 }
