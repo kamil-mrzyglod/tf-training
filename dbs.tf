18b79e2b8f56c62636bfec03f0ff4da3c7dc2c1d
Kamil Mrzygłód <kamil@thecloudtheory.com>
2022-05-10 14:50:25 +0200

    added DB

--- dbs.tf
+++ dbs.tf
@@ -1,8 +1,21 @@
 resource "azurerm_mssql_server" "training" {
-  name = "kamilm-tf-sqlserver"
-  resource_group_name = azurerm_resource_group.training.name
-  location = azurerm_resource_group.training.location
-  version = "12.0"
-  administrator_login = "test_admin"
+  name                         = "kamilm-tf-sqlserver"
+  resource_group_name          = azurerm_resource_group.training.name
+  location                     = azurerm_resource_group.training.location
+  version                      = "12.0"
+  administrator_login          = local.sql_login
   administrator_login_password = local.sql_admin_password
+}
+
+resource "azurerm_mssql_database" "training" {
+  name        = "kamilm-tf-sqldb"
+  server_id   = azurerm_mssql_server.training.id
+  sku_name    = "Basic"
+  max_size_gb = 2
+}
+
+resource "azurerm_key_vault_secret" "secret4" {
+  name = "sql-connection-string"
+  value = "Server=tcp:${azurerm_mssql_server.training.fully_qualified_domain_name},1433;Initial Catalog=${azurerm_mssql_database.training.name};Persist Security Info=False;User ID=${local.sql_login};Password=${local.sql_admin_password};MultipleActiveResultSets=True;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;"
+  key_vault_id = azurerm_key_vault.training.id
 }
--- locals.tf
+++ locals.tf
@@ -1,3 +1,4 @@
 locals {
-  sql_admin_password = "q1w2e3r4t5y6u7i8o9"
+  sql_login = "test_admin"
+  sql_admin_password = "<qH9DYQ$]J<bG}!]"
 }
